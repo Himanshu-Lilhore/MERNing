751df96dc64e7d52bd06e9814d54eaa0698f0702
himanshu-sec <himanshu.other.email@gmail.com>
2024-02-08 03:09:03 +0530

    Commit_08-Feb-24( 3:09)

--- autocommitWithMessage.bat
+++ autocommitWithMessage.bat
@@ -2,7 +2,7 @@
 set /p commit_message="Enter commit message: "
 
 git add .
-git commit -m "Commit_%date:/=-%(%time:~0,2%:%time:~3,2%) - %commit_message%"
+git commit -m "%commit_message%"
 git push
 
 pause
