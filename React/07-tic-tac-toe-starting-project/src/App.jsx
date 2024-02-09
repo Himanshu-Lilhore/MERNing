6d12222bed3d2b95d415d08dbc4450d3fec2bb22
himanshu-sec <himanshu.other.email@gmail.com>
2024-02-10 04:13:22 +0530

    till 95. Final Polishing & Improving Components

--- React/07-tic-tac-toe-starting-project/src/App.jsx
+++ React/07-tic-tac-toe-starting-project/src/App.jsx
@@ -14,50 +14,15 @@ const initialGameboard = [
 
 function calcActive(myTurns){
   let activeRN = 'X'
-
   if(myTurns.length !== 0 && myTurns[0].player === 'X'){
     activeRN = 'O'
   }
-
   return activeRN;
-
 }
 
 
-function App() {
-  const[turns, setTurns] = useState([]);
-  const[players, setPlayers] = useState({
-    'X': 'Himanshu',
-    'O': 'Hemant'
-  })
-  // const[active, setActive] = useState("X");
-  let active = calcActive(turns);
+function deriveWinner(gameboard, players){
   let winner
-  let drawn = false
-  let gameboard =  [...initialGameboard.map(arr => [...arr])]
-  
-  function handleRestart(){
-    setTurns([])
-  }
-  
-  function handlePlayerNameChange(symbol, newName){
-    setPlayers(prevPlayers => {
-      return({
-        ...prevPlayers,
-        [symbol]:newName    // this syntax is used when for key name we have a variable
-      });
-    }
-    )
-  }
-
-  for(const turn of turns){
-    const {square, player} = turn
-    const {row, col} = square
-    gameboard[row][col] = player;
-  }
-  
-  console.log(turns[0])
-
   for(const combination of WINNING_COMBINATIONS){
     let firstSq = gameboard[combination[0].row][combination[0].column]
     let secondSq = gameboard[combination[1].row][combination[1].column]
@@ -69,22 +34,65 @@ function App() {
         winner = players[firstSq];
     }
   }
+  return winner
+}
 
+
+function deriveGameboard(turns){
+  let gameboard =  [...initialGameboard.map(arr => [...arr])]
+  for(const turn of turns){
+    const {square, player} = turn
+    const {row, col} = square
+    gameboard[row][col] = player;
+  }
+  return gameboard
+}
+
+
+function App() {
+  const[turns, setTurns] = useState([]);
+  const[players, setPlayers] = useState({
+    'X': 'Himanshu',
+    'O': 'Hemant'
+  })
+  // const[active, setActive] = useState("X");  // dissolved state
+  
+  
+  function handleRestart(){
+    setTurns([])
+  }
+  
+
+  function handlePlayerNameChange(symbol, newName){
+    setPlayers(prevPlayers => {
+      return({
+        ...prevPlayers,
+        [symbol]:newName    // this syntax is used when for key name we have a variable
+      });
+    }
+    )
+  }
+  
+  
+  let active = calcActive(turns);
+  let gameboard = deriveGameboard(turns)
+  console.log(turns[0])
+  let drawn = false
+  let winner = deriveWinner(gameboard, players)
   if(!winner && turns.length === 9) drawn = true
+
 
   function handleSqClick(rowIndex, colIndex){
     // setActive((currActive) => currActive ==="X" ? "O" : "X" );
-
     setTurns(prevTurns => {
       let curPlayer = 'X';
-
       if(prevTurns.length > 0 && prevTurns[0].player === 'X'){
         curPlayer = 'O';
       }     // we could have written active directly in the return statement, but active is from other state so we get the latest state value (as of Turns state updation). 
-
       return ([{square:{row:rowIndex, col:colIndex}, player:curPlayer},...prevTurns]);
     });
   }
+
 
   return (
     <main>
